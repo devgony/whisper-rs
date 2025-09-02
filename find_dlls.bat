7b99850c6c6a99e1ab9415b2a3583d1fa8ffd03d
henry <devgony@gmail.com>
2025-09-02 11:29:31 +0900

    find_dlls from CARGO_TARGET_DIR

--- find_dlls.bat
+++ find_dlls.bat
@@ -2,13 +2,24 @@
 echo Searching for built DLL files...
 echo.
 
-for /r target\ %%f in (*.dll) do (
+:: Use CARGO_TARGET_DIR if set, otherwise default to target
+if defined CARGO_TARGET_DIR (
+    set TARGET_BASE=%CARGO_TARGET_DIR%
+    echo Using CARGO_TARGET_DIR: %CARGO_TARGET_DIR%
+) else (
+    set TARGET_BASE=target
+    echo Using default target directory
+)
+
+echo.
+echo Searching for DLL files in %TARGET_BASE%...
+for /r "%TARGET_BASE%\" %%f in (*.dll) do (
     echo Found: %%f
 )
 
 echo.
 echo DLLs should also be copied to:
-for /d %%d in (target\release\build\whisper-rs-sys-*) do (
+for /d %%d in ("%TARGET_BASE%\release\build\whisper-rs-sys-*") do (
     if exist "%%d\out\dll\" (
         echo %%d\out\dll\
         dir "%%d\out\dll\*.dll" /b 2>nul
